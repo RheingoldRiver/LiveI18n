86a2973fc1fb7d7a6490296ddfbfe7a448ed1608
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-05-15 14:44:07 -0500

    Don't use cldr cos I read documentation!

--- includes/LiveI18n.php
+++ includes/LiveI18n.php
@@ -6,7 +6,7 @@
  * @ingroup LiveI18n
  */
 
-use MediaWiki\Languages\LanguageNameUtils;
+use MediaWiki\Languages\LanguageFallback;
 use MediaWiki\MediaWikiServices;
 use PPFrame;
 
@@ -27,28 +27,20 @@ class LiveI18n {
 
 	public static function getOutput( $parser, $params ) {
 		$userLang = $parser->getOptions()->getUserLang();
+		$defaultLang = $GLOBALS["wgLiveI18nDefaultLanguageCode"];
 		if ( array_key_exists( $userLang, $params ) ) {
 			return $params[$userLang];
 		} else {
-			// check for cldr extension being installed
-			// if it is, then maybe we can do a better fallback than just the extension default
-			$cldr = is_callable( [ 'LanguageNames', 'getNames' ] );
-			if ($cldr) {
-				$fallbackLangs = LanguageNames::getNames( $userLang,
-					LanguageNames::FALLBACK_NORMAL,
-					LanguageNames::LIST_MW_AND_CLDR
-				);
-				foreach ($fallbackLangs as $attemptedLang) {
-					if (array_key_exists($attemptedLang, $params)) {
-						return $params[$attemptedLang];
-					}
+			$languageFallback = MediaWikiServices::getInstance()->getLanguageFallback();
+			$fallbacks = $languageFallback->getAll( $userLang, LanguageFallback::STRICT );
+
+			foreach ( $fallbacks as $attemptedLang ) {
+				if ( array_key_exists( $attemptedLang, $params ) ) {
+					return $params[$attemptedLang];
 				}
 			}
 
-			// if no cldr, or no acceptable preferred fallback was defined, then oh well,
-			// just return the default or else an empty string
-			$defaultLang = $GLOBALS["wgLiveI18nDefaultLanguageCode"];
-			return array_key_exists($defaultLang, $params) ? $params[$defaultLang] : '';
+			return array_key_exists( $defaultLang, $params ) ? $params[$defaultLang] : '';
 		}
 	}
 
